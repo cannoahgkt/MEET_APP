9aad5dd912f23649123929e9a642ec731d800671
Can Noah <87754848+Asareff@users.noreply.github.com>
2024-03-18 20:50:17 +0100

    4.5 commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
  return (
    <div className="App">
      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
       <EventList events={events} />
    </div>
  );
