6d6e75b6910a4731dc4dc5e259a53af13589975f
Can Noah <87754848+Asareff@users.noreply.github.com>
2024-03-20 17:19:08 +0100

    4.5 commits

--- src/mock-data.js
+++ src/mock-data.js
@@ -9,7 +9,127 @@ const mockData = [
         "kind": "calendar#event",
         "etag": "etag_value_2"
     },
-    // Add more objects as needed
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_3"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_4"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_5"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_6"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_7"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_8"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_9"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_10"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_11"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_12"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_13"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_14"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_15"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_16"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_17"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_18"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_19"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_20"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_21"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_22"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_23"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_24"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_25"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_26"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_27"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_28"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_29"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_30"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_31"
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "etag_value_32"
+    }
 ];
 
 export default mockData;
+
