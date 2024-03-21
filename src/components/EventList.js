44cd09d973b7e10ed199e26a9b1b1032e0ee5e91
Can Noah <87754848+Asareff@users.noreply.github.com>
2024-03-21 15:40:00 +0100

    new commits

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,15 +1,16 @@
 // src/components/EventList.js
 
+import React from "react";
 import Event from "./Event";
 
 const EventList = ({ events }) => {
- return (
-   <ul id="event-list">
-     {events ?
-       events.map(event => <Event key={event.id} event={event} />) :
-       null}
-   </ul>
- );
+  let numberOfEvents = events.slice(0, 5);
+
+  return (
+    <ul id="event-list">
+      {numberOfEvents.map(event => <Event key={event.id} event={event} />)}
+    </ul>
+  );
 }
 
 export default EventList;
